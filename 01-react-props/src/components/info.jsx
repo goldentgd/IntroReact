0e8177b9ade10b4dd144528ec5f7e8ff86078847
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-07-09 08:47:44 -0600

    página web

---
+++ 01-react-props/src/components/info.jsx
@@ -0,0 +1,11 @@
+const Info = ({ title, desc }) => {
+  return (
+    <section className="info">
+        <div className="contenedor">
+            <h2 className="titulo left">{title}</h2>
+            <p>{desc}</p>
+        </div>
+    </section>
+  )
+}
+export default Info
