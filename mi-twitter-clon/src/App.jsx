84fe14cab821fd7f0feacf76a5a77fd049596626
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-07-23 10:03:05 -0600

    Twitter clon

---
+++ mi-twitter-clon/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+
+import Profile from './pages/Profile'
+
+const App = () => {
+  return (
+
+    <Router>
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+
+        <Route path='/profile' element={<Profile />} />
+
+      </Routes>
+
+    </Router>
+
+  )
+}
+
+export default App
---
+++ mi-twitter-clon/src/components/Tweet.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const Tweet = ({ tweet, onLike }) => {
+  return (
+
+    <div className='tweet'>
+
+      <p>{tweet.text}</p>
+
+      <button onClick={() => onLike(tweet.id)}>
+
+        ❤ {tweet.likes}
+
+      </button>
+
+    </div>
+
+  )
+}
+
+export default Tweet
