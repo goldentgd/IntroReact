d0c61716fddaaa9531ebc8119e53870f983c3450
Adriana Camarillo Mora <adrianacamarillo@conceptosti.com>
2025-07-23 09:17:30 -0600

    citas-medicas

---
+++ citas-medicas/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Citas from './pages/Citas'
+import CitaDetalle from './pages/CitaDetalle'
+import NotFound from './pages/NotFound'
+
+function App () {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Inicio</Link>|{' '}
+        <Link to='/citas'>Ver Citas</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/citas' element={<Citas />} />
+        <Route path='/cita/:id' element={<CitaDetalle />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
